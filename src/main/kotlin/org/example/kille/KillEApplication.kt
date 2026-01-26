f66e76e9babe1cdcf2fa1ee01a1d8e22196db6a0
Gobziii <gobziii@yandex.ru>
2026-01-26 18:07:38 +0300

    Feat: Initialize project

---
+++ src/main/kotlin/org/example/kille/KillEApplication.kt
@@ -0,0 +1,11 @@
+package org.example.kille
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KillEApplication
+
+fun main(args: Array<String>) {
+    runApplication<KillEApplication>(*args)
+}
